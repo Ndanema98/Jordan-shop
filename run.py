31d2e16d4dea24fd62af3a10f774de395a732c4a
Ndanema98 <davidnda26@yahoo.co.uk>
2022-04-13 19:35:48 +0000

    Feat: Add validation to check whether exactly 12 values are entered.

--- run.py
+++ run.py
@@ -14,14 +14,35 @@ SHEET = GSPREAD_CLIENT.open("Jordan_palace")
 
 def get_sales_data():
     """
-    Request sales figures input from user. 
+    Request input of trainer name and sales figures for the year from user.
     """
-    print("Please enter sales data, for each trainer, from the last month.")
-    print("Data should be five numbers, seperated by commas.")
-    print("Example: 1,2,3,4,5\n")
+    print("Please enter trainer name")
+    name_str = input("Enter the name here: ")
+    print(f"The name provided is {name_str}\n")
+    
+
+    print("Please enter sales figures for the year.")
+    print("Data should be twelve numbers, seperated by commas.")
+    print("Example: 1,2,3,4,5,6,7,8,9,10,11,12\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Converts string values into integers.
+    Raises value error if string values can not be converted into integers,
+    or if there are not exactly 12 values. 
+    """
+    try:
+        if len(values) != 12:
+            raise ValueError(
+                f"Exactly 12 values are required, however you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 get_sales_data()
 
